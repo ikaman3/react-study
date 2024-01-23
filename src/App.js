99bd270ac1826bec351ace080cbce3e637f191a5
ikaman3 <yjhama1532@gmail.com>
2024-01-24 00:51:31 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-    return <button className="square" onClick={onSquareClick}>
-        {value}
-    </button>;
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
 }
 
 function Board({xIsNext, squares, onPlay}) {
